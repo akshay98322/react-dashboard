04e15551956cb6475028c088fcbdf90017665a5f
Akshay Das <akshay98322@gmail.com>
2022-06-23 13:21:18 +0530

    Added the seetings icon and sidebar logic

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 npm install --legacy-peer-deps
+git add -A
+git commit -m "Added the seetings icon and sidebar logic"
--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings} from 'react-icons/fi';
+import { Tooltip, TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+
+import './App.css'
+import { Button } from '@syncfusion/ej2/buttons';
 
 const App = () => {
+  const activeMenu = true;
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+      <div className="flex relative dark:bg-main-dark-bg">
+        <div className='fixed right-4 bottom-4' style={{ zIndex: '1000'}} >
+          <TooltipComponent content='Settings' position='Top'>
+            <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text:white' style={{ background: 'blue', borderRadius: '50%'}}>
+              <FiSettings/>
+            </button>
+
+          </TooltipComponent>
+
+        </div>
+        {activeMenu ? (
+        <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>Sidebar</div>
+        ) : (
+        <div className='w-0 dark:bg-secondary-dark-bg'>Sidebar w-0</div>)}
+      </div>
+      </BrowserRouter>
+    </div>
+    
   )
 }
 
